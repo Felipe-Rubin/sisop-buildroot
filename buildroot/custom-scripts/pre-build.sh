3453b438f99004ac975a3830d472f44ac420c44f
Felipe Rubin <felipeprubin@gmail.com>
2018-08-20 22:44:28 -0300

    Passwordless ssh

--- buildroot/custom-scripts/pre-build.sh
+++ buildroot/custom-scripts/pre-build.sh
@@ -3,3 +3,7 @@
 #Configure network
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+#Configure Dropbear
+cp $BASE_DIR/../custom-scripts/S41nosshpass-config $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S41nosshpass-config
